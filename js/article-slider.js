a49892adb812bf678ee58b154eb52a0eebf2575f
dwpierce <dwpierce@wsu.edu>
2016-09-27 14:08:35 -0700

    slider update

--- js/article-slider.js
+++ js/article-slider.js
@@ -42,21 +42,23 @@ var slide_show = function(container) {
 				var selectedNum = $selectedClass.substr($selectedClass.lastIndexOf("-")+1);
 /*				var pageida = '.page-item-id-' + selectNum + ' > a';	*/
 				 
-
 				 
 /*				 jQuery('div').find("[data-id='" + $selectedNum + "']") */
 				jQuery('.active').removeClass('active'); 
 				
 /*				jQuery('#events-title').removeClass('active'); */
 				
-				jQuery("div").find("[data-id='" + selectedNum + "']").addClass('active');
+				jQuery("div").find("[data-id='" + selectedNum + "']").addClass('active');  
+				
+							var slug = jQuery('.slide').attr("id");			
+							alert(slug);
+			
 				
 				jQuery('.current_page_item').removeClass('current_page_item');	
 							
 			    jQuery(this).parent().addClass('current_page_item'); 
-/*
-							var slug = jQuery('div').attr("id");			
-							var _href = jQuery("pageida").attr("href");
+
+/*							var _href = jQuery("pageida").attr("href");
 								jQuery("pageida").attr("href",  _href + slug);
 */	
 				/* jQuery(this).parent().addClass('current_page_item'); */
