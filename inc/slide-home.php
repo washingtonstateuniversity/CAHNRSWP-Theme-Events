bacd82a6e4f0acbf739610d605f5e4982fae7bf3
dwpierce <dwpierce@wsu.edu>
2016-09-27 17:20:54 -0700

    slide-home.php update

--- inc/slide-home.php
+++ inc/slide-home.php
@@ -5,8 +5,8 @@
             <h3> Event Date Here</h3><p />
             <h3>Event Location Goes Here <a href="#">[Map link]</a></h3><p /> 	
             <div class="rsvp"><a href="#">[RSVP]</a></div>
+            <p><?php the_content(); ?></p>
         </div>
-        <p><?php the_content(); ?></p>
-    </div>
+   </div>
 </article>
 
